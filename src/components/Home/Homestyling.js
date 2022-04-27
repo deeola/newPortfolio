073160f2d5f1de17f1a67906e4e2a29983c2b8b3
Adeola Bamigboye <clazikar@gmail.com>
2022-04-27 16:37:02 +0200

    add hover style

--- src/components/Home/Homestyling.js
+++ src/components/Home/Homestyling.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 //Home
 export const HomeContainer = styled.div`
@@ -16,6 +17,7 @@ export const LeftContainerDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+ 
 `;
 
 export const LogoHamContainer = styled.div`
@@ -30,6 +32,7 @@ export const LogoContainer = styled.div`
   width: 60px;
   display: flex;
   align-items: center;
+  cursor: Pointer;
 
   img {
     display: block;
@@ -41,6 +44,7 @@ export const HamContainer = styled.div`
   width: 50px;
   display: flex;
   align-items: center;
+  cursor: pointer;
 
   img {
     display: block;
@@ -58,17 +62,17 @@ export const MyName = styled.div`
   color: white;
 
   h1 {
-      font-size: 60px;
+    font-size: 60px;
   }
 `;
 
 export const MyAbout = styled.div`
-margin-top: 20px;
+  margin-top: 20px;
   color: white;
   p {
-      line-height: 25px;
+    line-height: 25px;
     span {
-        color: #5390d9;
+      color: #5390d9;
     }
   }
 `;
@@ -78,90 +82,187 @@ export const LanguageOptions = styled.div`
   margin: 0px auto;
   display: flex;
   align-items: center;
-margin-top: 40px;
+  margin-top: 40px;
 `;
 
 export const EmptyDiv = styled.div`
-  background-color: #5390d9 ;
+  background-color: white;
   width: 3px;
   height: 100px;
+  visibility: hidden;
 `;
 
 export const LanguageDiv = styled.div`
   display: flex;
   margin-left: 40px;
 
-  img{
-      width: 30px;
+  img {
+    width: 30px;
   }
 
-  p{
-      margin-left: 10px;
-      color: white;
-
+  p {
+    margin-left: 10px;
+    color: white;
   }
 `;
 
-
-export const Bottom = styled.div`
-
-`
-
-
+export const Bottom = styled.div``;
 
 //MiddleContainer
 
+export const TopContainer = styled.div`
+  p {
+    color: #5390d9;
+    margin-top: 6px;
+  }
+`;
+
+export const BottomContainer = styled.h1`
+  font-size: 70px;
+  font-weight: 700;
+  color:  #5390d9;
+`;
+
 export const MiddleContainerDiv = styled.div`
   width: 33.3333333333%;
-  background-color: #5390d9;
+
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  background-color: #386cab;
+  cursor:pointer;
+  transition: 300ms ease-in;
+  &:hover {
+    background-color:  #5390d9;
+  
+  }
+
+  &:hover ${BottomContainer} {
+    color: white;
+  
+  }
+
+  &:hover ${TopContainer } {
+
+    p{
+        color: white;
+   
+    }
+   
+  }
+
  
 `;
 
 export const ProjectContainer = styled.div`
-width: 85%;
-margin: 0px auto;
-`
+  width: 85%;
+  margin: 0px auto;
+`;
 
-export const TopContainer = styled.div``
 
-export const BottomContainer = styled.h1`
-font-size: 70px;
-font-weight: 700;
-
-`
 
 export const BottomMiddleContainer = styled.div`
-width: 85%;
-margin: 0px auto;
-margin-bottom: 36px;
-display: flex;
-align-items: right;
-background-color: green;
-position: relative;
-
-`
+  width: 85%;
+  margin: 0px auto;
+  margin-bottom: 36px;
+  display: flex;
+  align-items: right;
+  background-color: green;
+  position: relative;
+`;
 
 export const DownloadCV = styled.a`
+  color: white;
+  background-color: none;
+  border: 1px solid white;
+  padding: 15px 15px;
+  text-decoration: none;
+  position: absolute;
+  right: 0;
+  bottom: 5px;
+  transition: 300ms ease-in;
 
-color: white;
-background-color: none;
-border: 1px solid white;
-padding: 10px 15px;
-text-decoration: none;
-position: absolute;
-right: 0;
-bottom: 5px;
-
-
-`
+  &:hover {
+    background-color:  #386cab;
+    border: none;
+  }
+`;
 
 //RightContainer
+
+export const Icon = styled(FontAwesomeIcon)`
+  color: #386cab;
+  font-size: 30px;
+  cursor: pointer;
+`;
+
+export const EmptyRightDiv = styled.div`
+  width: 35%;
+  margin: 0px auto;
+  margin-bottom: 36px;
+  display: flex;
+  align-items: right;
+  justify-content: space-between;
+  background-color: none;
+  border: 1px solid #386cab;
+  padding: 10px 15px;
+
+  display: flex;
+  position: absolute;
+  left: 0;
+  bottom: 5px;
+
+  
+
+ 
+`;
+
+
 
 export const RightContainerDiv = styled.div`
   width: 33.3333333333%;
   background-color: white;
-  
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+
+  cursor: pointer;
+  transition: 300ms ease-in;
+
+  &:hover {
+    background-color:  #5390d9;
+  }
+
+  &:hover ${BottomContainer} {
+    color: white;
+  }
+
+  &:hover ${TopContainer } {
+
+    p{
+        color: white;
+    }
+   
+  }
+
+  &:hover ${Icon} {
+    color: white;
+  }
+
+  &:hover ${EmptyRightDiv} {
+    color: white;
+    border: 1px solid white;
+  }
+`;
+
+
+
+export const BottomRightContainer = styled.div`
+  width: 85%;
+  margin: 0px auto;
+  display: flex;
+  align-items: right;
+  background-color: green;
+  position: relative;
 `;
--- src/components/Home/RightContainer.js
+++ src/components/Home/RightContainer.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { RightContainerDiv } from './Homestyling';
+import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  BottomContainer,
+  BottomRightContainer,
+  EmptyDiv,
+  ProjectContainer,
+  TopContainer,
+  RightContainerDiv,
+  Icon,
+  EmptyRightDiv,
+} from "./Homestyling";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fab, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab);
 
 const RightContainer = () => {
   return (
-    <RightContainerDiv>RightContainer</RightContainerDiv>
-  )
-}
+    <RightContainerDiv>
+      <ProjectContainer>
+        <TopContainer>
+          <EmptyDiv />
+          <p>LEARN MORE ABOUT ME</p>
+        </TopContainer>
+        <BottomContainer>ABOUT</BottomContainer>
+      </ProjectContainer>
+      <BottomRightContainer>
+        <EmptyRightDiv>
+        <Icon icon={faLinkedin} />
+        <Icon icon={faGithub} />
+        </EmptyRightDiv>
+        
+        <FontAwesomeIcon />
+      </BottomRightContainer>
+    </RightContainerDiv>
+  );
+};
 
 export default RightContainer;
